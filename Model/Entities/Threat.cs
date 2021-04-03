4e388f038c2ad5fd79cffe156ee24f509dc085e1
maximborodkin <maxim.borodkin1999@yandex.ru>
2021-04-03 17:57:49 +0300

    base structure created

---
+++ Model/Entities/Threat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreatParser.Model.Entities
+{
+    public class Threat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Source { get; set; }
+        public string InteractionObject { get; set; }
+        public bool ConfidentialityBreach { get; set; }
+        public bool IntegrityBreach { get; set; }
+        public bool AvailabilityBreach { get; set; }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, Description, Source, InteractionObject, ConfidentialityBreach, IntegrityBreach, AvailabilityBreach);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Threat another = obj as Threat;
+            return another != null
+                && Id == another.Id
+                && Name == another.Name
+                && Description == another.Description
+                && Source == another.Source
+                && InteractionObject == another.InteractionObject
+                && ConfidentialityBreach == another.ConfidentialityBreach
+                && IntegrityBreach == another.IntegrityBreach
+                && AvailabilityBreach == another.AvailabilityBreach;
+        }
+    }
+}
---
+++ Model/ThreatsRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThreatParser.Model.Entities;
+
+namespace ThreatParser.Model
+{
+    public static class ThreatsRepository
+    {
+        public static List<Threat> Threats { get; private set; }
+        private static string fileUrl = @"https://bdu.fstec.ru/files/documents/thrlist.xlsx";
+        private static string localCachePath = @"/threats_list.csv";
+
+        static ThreatsRepository()
+        {
+            Threats = new List<Threat>();
+        }
+
+        public static List<Threat> LoadFromDisk()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool DownloadFile(out int progress)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<Threat> ParseFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
